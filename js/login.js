658356cf97d292b43ba1e70d0c2677d5c4252d43
Daniel Vitor <danielvitorlog221@gmail.com>
2022-04-26 17:01:17 -0300

    Atualização

--- js/login.js
+++ js/login.js
@@ -61,18 +61,3 @@ function myFunction() {
     }
 
 //Teste Registro
-
-var dropdown = document.getElementsByClassName("btnRegistar");
-var i;
-    
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = document.getElementsByClassName("register_container");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-    });
-}
