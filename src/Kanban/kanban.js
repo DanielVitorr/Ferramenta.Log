430d7268a4c98723872fad47001d48febbf56661
Daniel Vitor <danielvitorlog221@gmail.com>
2022-05-19 09:12:55 -0300

    kanban 2.0

--- src/Kanban/kanban.js
+++ src/Kanban/kanban.js
@@ -1,8 +1,35 @@
-import KanbanAPI from "./kanbanAPI.js";
+import Column from "./colunas.js";
 
-KanbanAPI.updateItem(123,{
-    columnId: 1 ,
-    position: 0,
-    content: "eu mudei"
+export default class Kanban {
+    constructor(root){
+        this.root = root;
 
-});
+        Kanban.columns().forEach(column => {
+            //todo:ccira uma classe de coluna instantaneo
+            const columnView = new Column (column.id,column.title);
+
+            this.root.appendChild(columnView.elements.root);
+        });
+    }
+
+    static columns(){
+        return [
+            {
+                id: 12, 
+                title : "Não iniciadas"
+            },
+            {
+                id: 13 ,
+                title : "Em andamento"
+            },
+            {
+                id: 14,
+                title : "Atrasadas"
+            },
+            {
+                id: 15,
+                title : "Finalizadas"
+            }
+        ];
+    }
+}
